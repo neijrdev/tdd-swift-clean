5eba5d012f684a407b5758a03b0e66befe4c9d7b
n.junior@paylivre.com <n.junior@paylivre.com>
2024-03-01 12:33:18 -0300

    Criado AddAccount

---
+++ CleanApp/Domain/AddAccont.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+}
+
+struct AddAccountModel{
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
+
+struct AccountModel{
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
