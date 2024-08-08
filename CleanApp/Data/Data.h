db5272f1cc9a1b8911e00aa4dc13d144811315e1
neijunior <n.junior@palivre.com>
2024-08-08 10:32:45 -0300

    add data layer

---
+++ CleanApp/Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by NEI VITOR on 08/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
